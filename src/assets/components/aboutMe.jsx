a1a7e3c2bf77e1e999c929d6f5ad66bca80e6f5f
[Ashley Braxton] <[ashley.braxton25@gmail.com]>
2023-08-16 18:49:00 -0400

    started working on how portfolio should look added header, footer, and navbar components

---
+++ src/assets/components/aboutMe.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+    return(
+        <div className='about-me'>
+            <h2>About Me</h2>
+            <img src="./src/assets/avatar.png" alt="female avatar" />
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, temporibus consequatur, illum ducimus alias, mollitia cumque labore quasi repudiandae beatae excepturi dicta vitae maiores itaque architecto molestiae corrupti est perspiciatis voluptates? Et aspernatur consequuntur culpa quod adipisci nostrum esse consectetur molestias atque veniam laboriosam maiores voluptates, eius autem, delectus veritatis.</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, temporibus consequatur, illum ducimus alias, mollitia cumque labore quasi repudiandae beatae excepturi dicta vitae maiores itaque architecto molestiae corrupti est perspiciatis voluptates? Et aspernatur consequuntur culpa quod adipisci nostrum esse consectetur molestias atque veniam laboriosam maiores voluptates, eius autem, delectus veritatis.</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/assets/components/header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Nav from "./Nav";
+
+function Header(props) {
+  const { currentTab, handleTabChange } = props;
+
+  return (
+    <div className='header'>
+      <section>
+        <header className="flex-row px-1">
+          <div>
+            <h1>Ashley Braxton</h1>
+            <Nav
+              currentTab={currentTab}
+              handleTabChange={handleTabChange}
+            ></Nav>
+          </div>
+        </header>
+      </section>
+    </div>
+  );
+}
+
+export default Header;
