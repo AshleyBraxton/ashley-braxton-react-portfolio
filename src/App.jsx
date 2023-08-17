a7b9d77f809c76e5a2d10b2d87743f56b8e71d17
[Ashley Braxton] <[ashley.braxton25@gmail.com]>
2023-08-16 23:56:33 -0400

    created portfolio page component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './assets/components/header';
 import About from './assets/components/aboutMe';
 import Footer from './assets/components/footer';
+import Portfolio from './assets/components/portfolio';
 // import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -10,7 +11,10 @@ function App() {
   return (
     <>
     <Header />
-    <About />
+    <div className='main-content'>
+    {/* <About /> */}
+    <Portfolio />
+    </div>
     <div>
     <Footer />
     </div>
